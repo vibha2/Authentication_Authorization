f2af769309a0f74d31c4488c2c6d502f42ebf855
vibha2 <juhisahu2203@gmail.com>
2023-06-07 02:21:27 +0530

    login validations created

--- Controllers/Auth.js
+++ Controllers/Auth.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 //signup route handler
 exports.signup = async(req,res) => {
@@ -55,3 +57,66 @@ exports.signup = async(req,res) => {
 //     "password":"iamvibha",
 //     "role":"Admin"
 // }
+
+
+//login handler
+exports.login = async(req,res) => {
+    try{
+        //data fetch
+        const {email, password} = req.body;
+
+        //validation on email and password
+        if(!email || !password) {
+            return res.status(400).json({
+                success:false,
+                message:'Please fill all the details carefully',
+
+            })
+            
+        }
+
+        //check for registered user
+        const user = await User.findOne({email});
+        //if not a registered user
+        if(!user){
+            return res.status(401).json({
+                success:false,
+                message:'User is not registered',
+
+            });
+        }
+
+        //data we want to sent in token
+        const payload = {
+            email: user.email,
+            id:user._id,
+            role:user.role,
+        };
+        //verify password & generate a JWT Token
+        if(await bcrypt.compare(password, user.password))
+        {
+            //password matches then login
+            //create token
+            let token = jwt.sign(payload,
+                process.env.JWT_SECRET,
+                {
+                    expiresIn:"2h",
+                });
+
+
+        }
+        else{
+            //password do not match
+            return res.status(403).json({
+                success:false,
+                message:'Password Incorrect',
+
+            });
+
+        }
+    }
+    catch(error)
+    {
+
+    }
+}
